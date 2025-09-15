e7e1f13819ebbca489ba0e37f09f47072866dca1
guilherme <guilhermetorres1401@gmail.com>
2025-09-15 12:45:40 -0300

    mudança no prompt para passar a planilha de atos compilados

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import os
 import requests
 import redis
 from dotenv import load_dotenv
+import pandas as pd
 from google import genai
 from PyPDF2 import PdfReader
 from flask import Flask
@@ -58,7 +59,7 @@ def load_pdf(filename: str) -> str:
         texto_em_cache = r.get(filename)
         if texto_em_cache:
             return texto_em_cache
-        print("texto não encontrado em cache. Extraindo texto do pdf...")
+        print(f"texto não encontrado em cache. Extraindo texto do pdf: {filename}")
         raw_text = ""
         file_path = os.path.join(download_dir, filename)
         pdfreader = PdfReader(file_path)
@@ -78,39 +79,29 @@ def load_pdf(filename: str) -> str:
 
 client = genai.Client()
 
+df = pd.read_csv(os.getenv("SHEETS_URL"))
+atos_compilados = df["Nº da Lei/ Decreto"].values
+
 def make_rag_prompt(text: str):
     prompt = f"""
-Você é um assistente jurídico especializado na análise de atos normativos. Receberá o texto do Diário Oficial do Estado do Piauí (DOEPI), que contém publicações oficiais do dia.
-
-Sua tarefa é identificar **quais Leis, Decretos ou Portarias foram mencionadas como alteradas, revogadas ou regulamentadas no DOEPI**.
-
-### Regras para análise:
-
-* Considere como **alterado** qualquer ato que tenha sido **modificado, revogado total ou parcialmente, substituído ou regulamentado**.
-* A alteração pode ocorrer de forma explícita ou implícita, como nos casos em que um novo ato menciona alterar, revogar ou regulamentar um anterior.
-* Ignore republicações sem alterações, homenagens ou meras citações sem efeito normativo.
-* Analise apenas **Leis**, **Decretos** e **Portarias**.
+Este Diário Oficial alterou algum dos atos compilados referidos na tabela? Se Sim, quais?
 
 ### Entrada:
 
-**Texto do DOEPI:**
+**Lista de Atos Compilados:**
+{"\n".join([f"- {ato_compilado}" for ato_compilado in atos_compilados])}
+
+**Texto do Diário Oficial:**
+```
 {text}
-
-### Saída esperada (formato de resposta):
-
-Liste apenas os atos que foram alterados, com breve justificativa extraída do DOEPI:
-
-**Atos alterados encontrados no DOEPI:**
-
-* **Decreto nº 10.123/2020** - Alterado pelo Decreto nº 10.987/2025.
-* **Portaria nº 58/2023** - Revogada conforme publicação no DOEPI.
+```
     """
     return prompt
 
 
 def generate_answer(prompt: str):
     result = client.models.generate_content(
-        model="gemini-2.0-flash", contents=prompt
+        model=os.getenv("AI_MODEL"), contents=prompt
     )
     return result.text
 
@@ -131,6 +122,7 @@ def fetch_last_doe():
 @app.route("/analyze-last-doe", methods=["POST"])
 def analyze_last_doe():
     filename = download_last_doe()
+    # filename = "DOEPI_120_2025.pdf"
     pdf_text = load_pdf(filename)
     prompt = make_rag_prompt(pdf_text)
     ai_response = generate_answer(prompt)
